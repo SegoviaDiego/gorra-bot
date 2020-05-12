2dbd7154f6fca1391df91821ce47b3181ff9f3f1
wzfc <wzfernandochen@gmail.com>
2020-05-12 19:56:57 -0300

    feat: 🎸 configure MongoDB

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,30 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    MongooseModule.forRootAsync({
+      useFactory: async () => {
+        if (process.env.NODE_ENV === 'production') {
+          return {
+            uri: process.env.MONGO_URI,
+            useNewUrlParser: true
+          };
+        } else {
+          const { MongoMemoryServer } = await import('mongodb-memory-server');
+          const { default: mongoose } = await import('mongoose');
+
+          mongoose.set('debug', true);
+          const mongod = new MongoMemoryServer();
+          const uri = await mongod.getUri();
+
+          return { uri };
+        }
+      }
+    })
+  ],
   controllers: [AppController],
   providers: [AppService]
 })
